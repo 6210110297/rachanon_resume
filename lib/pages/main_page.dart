3a761ab20bfeece59240dcded603bf8b33af7c40
rachanon khongchai <6210110297@psu.ac.th>
2021-11-26 14:31:06 +0700

    add Text and image in main page

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -3,20 +3,71 @@ import 'package:flutter/material.dart';
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
         Container(
-          margin: EdgeInsets.all(35),
-          alignment: Alignment.topCenter,
-          child: Text(
-            'Rachanon Khongchai',
-            style: Theme.of(context).textTheme.headline1,
+          height: 300,
+          color: Color.fromRGBO(255, 255, 255, 0.5),
+          padding: EdgeInsets.all(20),
+          alignment: Alignment.centerLeft,
+          // decoration: BoxDecoration(
+          //   color: Colors.white,
+          //   image: DecorationImage(
+          //     image: NetworkImage(
+          //         'https://raw.githubusercontent.com/6210110297/rachanon_resume/master/assets/pictures/cool_pic.jpg'),
+          //     fit: BoxFit.contain,
+          //   ),
+          // ),
+          child: Row(
+            children: [
+              Container(
+                child: Image.network(
+                    'https://raw.githubusercontent.com/6210110297/rachanon_resume/master/assets/pictures/cool_pic.jpg'),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Rachanon Khongchai',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline1!
+                      .apply(color: Colors.black),
+                ),
+              ),
+            ],
           ),
         ),
         Container(
-          padding: EdgeInsets.all(20),
-          // child: NetworkImage(),
-        )
+          alignment: Alignment.topCenter,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: <Color>[Colors.blue[800]!, Colors.pink[800]!])),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                'Info',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+              Text(
+                'About me',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+              Text(
+                'Pupa',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+              Text(
+                'Skill/Experience',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+              Text(
+                'Event',
+                style: Theme.of(context).textTheme.headline1,
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
